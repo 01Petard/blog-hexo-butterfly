8030cedf92e2679b81aaad680a478220a3c0e0a8
01Petard <1520394133@qq.com>
2024-10-27 16:00:46 +0800

    fix: 修改了自动化脚本

--- git_pull_deploy_operations.sh
+++ git_pull_deploy_operations.sh
@@ -16,13 +16,8 @@ git pull gitee china:china
 echo "========================================"
 
 # 清理 Hexo
-echo "清理 Hexo..."
-hexo clean
-echo "========================================"
-
-# 生成静态文件
-echo "生成静态文件..."
-hexo g
+echo "清理 Hexo，并重新生成静态文件..."
+hexo clean && hexo g
 echo "========================================"
 
 # 停止 pm2 进程
@@ -35,4 +30,5 @@ echo "启动 pm2 进程..."
 pm2 start hexo_run.cjs
 echo "========================================"
 
-echo "所有操作已完成。"
+echo "博客已重新部署，详情点击： http://www.huangzexiao.top"
+
